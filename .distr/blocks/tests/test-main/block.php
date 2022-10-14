3c0c2ccd7dc0317f1364de276ee4877778be0b9f
Драницын Богдан Викторович <bdranitsyn@synergy.ru>
2022-10-14 17:08:44 +0300

    tests responsive

--- .distr/blocks/tests/test-main/block.php
+++ .distr/blocks/tests/test-main/block.php
@@ -1,6 +1,8 @@
 <section class="test-main section test-main__section">
 	<div class="container test-main__container">
+		<h2 class="test-main__test-title _mob">Название теста</h2>
 		<div class="test-main__img">
+			<img src="img/tests/test-main/test_img.png" alt="">
 		</div>
 		<div class="test-main__test">
 			<h2 class="test-main__test-title">Название теста</h2>
--- .distr/blocks/tests/test-video/block.php
+++ .distr/blocks/tests/test-video/block.php
@@ -2,12 +2,14 @@
 	<div class="container test-video__container">
 		<h2 class="test-video__title">Видео (название видео)</h2>
 		<div class="test-video__video">
-			<a href="" class="test-video__video-start">
-				<img src="img/test-video/img.png" alt="">
-			</a>
-			<div class="test-video__video-btn">
-				<a href="" class="test-video__video-btn_all button">Все видео</a>
-				<a href="" class="test-video__video-btn_miss button">Пропустить</a>
+			<div class="test-video__video-box">
+				<a href="" class="test-video__video-start">
+					<img src="img/test-video/img.png" alt="">
+				</a>
+				<div class="test-video__video-btn">
+					<a href="" class="test-video__video-btn_all button">Все видео</a>
+					<a href="" class="test-video__video-btn_miss button">Пропустить</a>
+				</div>
 			</div>
 		</div>
 </section>
